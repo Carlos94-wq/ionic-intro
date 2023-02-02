5f6a8243a0c860ee17552d3667f19f7cf3f42097
Carlos Rodriguez <carlos.ed702@gmail.com>
2023-02-01 22:38:24 -0600

    inicio de curso

---
+++ src/app/pages/inicio/inicio.page.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inicio',
+  templateUrl: './inicio.page.html'
+})
+export class InicioPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
